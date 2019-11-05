1a77982ea9a661d326323678b03c662462859ae8
David Lopez Ammirata <david.lopez.ammirata@gmail.com>
2019-11-05 12:50:37 -0600

    email final

--- mail_handler.php
+++ mail_handler.php
@@ -3,7 +3,7 @@ if(isset($_POST['submit'])){
     $to = "staff@robofestlatam.org, r.rodriguez@robofestlatam.org, contacto@robofestlatam.org, r.marriro@robofestlatam.org"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['first_name'];
-    $subject = "Form submission";
+    $subject = $_POST['subject'];
     $subject2 = "Copia de tu mensaje";
     $message = $first_name . " Dice:" . "\n\n" . $_POST['message'];
     $message2 = "Copia de tu mensaje " . $first_name . "\n\n" . $_POST['message'];
